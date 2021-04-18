4a0148b8b36e0d3981d3870160641d0fa2d2764f
전병훈 <ifries8611@gmail.com>
2021-04-18 14:15:33 +0900

    API를 통한 날씨정보 추가

--- target/chromeApp-1.0-SNAPSHOT/weather.js
+++ target/chromeApp-1.0-SNAPSHOT/weather.js
@@ -1,12 +1,30 @@
+const weather = document.querySelector(".js-weather");
+
+
 const API_KEY = "cb5d1c4c3dbe915de9873691eb289273";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    // request
+    // then : 데이터가 완전히 들어온 다음에 인자의 function호?
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+
+        weather.innerHTML = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function askForCoords() {
-    navigator.geolocation.getCurrentPosition(function(position){
+    navigator.geolocation.getCurrentPosition(function (position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         const coordsObj = {
@@ -14,18 +32,23 @@ function askForCoords() {
             longitude
         }
         saveCoords(coordsObj);
+        getWeather(latitude, longitude);
 
-    }, function(){
+    }, function () {
         console.log('can not access geo location');
     });
 }
 
 function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
+
+    console.log(loadedCoords);
     if (loadedCoords === null) {
         askForCoords();
     } else {
         // get Weather
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
