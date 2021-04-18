a72e9b1023076811ee1115cd6e1915c28a13d012
전병훈 <ifries8611@gmail.com>
2021-04-18 10:28:47 +0900

    todo 리스트 localStorage 저장 및 브라우저에 저장한 todo list render

--- src/main/webapp/todo.js
+++ src/main/webapp/todo.js
@@ -5,8 +5,16 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos() {
+    // localStorage에 저장되는 모든 데이터는 String으로 저장해야함.
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text){
     const li = document.createElement("li");
+    const newId = toDos.length + 1;
 
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
@@ -16,14 +24,28 @@ function paintToDo(text){
 
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
 
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    }
+
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function loadTodos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
+    const loadToDos = localStorage.getItem(TODOS_LS);
+    if (loadToDos !== null) {
+        // localStorage에서 가져온 데이터는 String이기 때문에 다시 JSON으로 변경해줘야함
+        const parsedTodos = JSON.parse(loadToDos);
 
+        parsedTodos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        })
     }
 }
 
